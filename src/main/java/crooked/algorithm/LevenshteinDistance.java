7ecb31054d4375152a1939cd63bbf56f7478385c
Marcelo Oikawa <greatjapa@gmail.com>
2017-08-13 16:36:11 -0300

    draft version of levenshtein distance

--- src/main/java/crooked/algorithm/LevenshteinDistance.java
+++ src/main/java/crooked/algorithm/LevenshteinDistance.java
@@ -12,6 +12,27 @@ public class LevenshteinDistance implements IStringDistance {
         Objects.requireNonNull(first, "first word should not be null.");
         Objects.requireNonNull(second, "second word should not be null.");
 
-        return 0;
+        int[][] distance = new int[first.length() + 1][second.length() + 1];
+
+        for (int i = 0; i <= first.length(); i++) {
+            distance[i][0] = i;
+        }
+        for (int j = 1; j <= second.length(); j++) {
+            distance[0][j] = j;
+        }
+        for (int i = 1; i <= first.length(); i++) {
+            for (int j = 1; j <= second.length(); j++) {
+
+                distance[i][j] = min(
+                        distance[i - 1][j] + 1,
+                        distance[i][j - 1] + 1,
+                        distance[i - 1][j - 1] + ((first.charAt(i - 1) == second.charAt(j - 1)) ? 0 : 1));
+            }
+        }
+        return distance[first.length()][second.length()];
+    }
+
+    private int min(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
     }
 }
