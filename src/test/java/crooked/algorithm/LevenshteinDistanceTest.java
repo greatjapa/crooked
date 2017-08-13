d9f8fa4f0950a43c093da49ff761e72b00b650bf
Marcelo Oikawa <greatjapa@gmail.com>
2017-08-13 16:44:52 -0300

    adding more unit tests

--- src/test/java/crooked/algorithm/LevenshteinDistanceTest.java
+++ src/test/java/crooked/algorithm/LevenshteinDistanceTest.java
@@ -26,7 +26,7 @@ public class LevenshteinDistanceTest {
     }
 
     @Test
-    public void testEqualWords() {
+    public void testSameWords() {
         IStringDistance algorithm = new LevenshteinDistance();
         Assert.assertEquals(0, algorithm.calc("book", "book"));
     }
@@ -47,5 +47,53 @@ public class LevenshteinDistanceTest {
     public void testInfixInsertion() {
         IStringDistance algorithm = new LevenshteinDistance();
         Assert.assertEquals(1, algorithm.calc("bok", "book"));
+    }
+
+    @Test
+    public void testDeleteAtEnd() {
+        IStringDistance algorithm = new LevenshteinDistance();
+        Assert.assertEquals(1, algorithm.calc("book", "boo"));
+    }
+
+    @Test
+    public void testDeleteAtBeginning() {
+        IStringDistance algorithm = new LevenshteinDistance();
+        Assert.assertEquals(1, algorithm.calc("book", "ook"));
+    }
+
+    @Test
+    public void testInfixDeletion() {
+        IStringDistance algorithm = new LevenshteinDistance();
+        Assert.assertEquals(1, algorithm.calc("book", "bok"));
+    }
+
+    @Test
+    public void testReplacementAtEnd() {
+        IStringDistance algorithm = new LevenshteinDistance();
+        Assert.assertEquals(1, algorithm.calc("book", "boot"));
+    }
+
+    @Test
+    public void testReplacementAtBeginning() {
+        IStringDistance algorithm = new LevenshteinDistance();
+        Assert.assertEquals(1, algorithm.calc("book", "took"));
+    }
+
+    @Test
+    public void testInfixReplacement() {
+        IStringDistance algorithm = new LevenshteinDistance();
+        Assert.assertEquals(1, algorithm.calc("book", "brok"));
+    }
+
+    @Test
+    public void testDifferentWordsSameLength() {
+        IStringDistance algorithm = new LevenshteinDistance();
+        Assert.assertEquals(4, algorithm.calc("book", "take"));
+    }
+
+    @Test
+    public void testDifferentWords() {
+        IStringDistance algorithm = new LevenshteinDistance();
+        Assert.assertEquals(5, algorithm.calc("book", "apple"));
     }
 }
